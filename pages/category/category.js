08bf89f27defeedb965a6ac19d666d34155d162f
renyinyu <471630805@qq.com>
2020-12-15 21:44:26 +0800

    商品分类页

--- pages/category/category.js
+++ pages/category/category.js
@@ -14,7 +14,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.fetchGoodsCategory()
+    const goodsCategory = wx.getStorageSync('goodsCategory')
+    console.log('ddd,', goodsCategory)
+    const expireSeconds = 1000 * 10
+    const nowDate = Date.now()
+    if (goodsCategory && nowDate - goodsCategory.time < expireSeconds) {
+      this.setData({
+        asideData: goodsCategory.data,
+        activeAsideItem: goodsCategory.data[0]
+      })
+    } else {
+      this.fetchGoodsCategory()
+    }
   },
 
   /**
@@ -24,7 +35,10 @@ Page({
     request({
       url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
     }).then(data => {
-      console.log('商品分类，', data)
+      wx.setStorage({
+        data: {time: Date.now(), data: data.message},
+        key: 'goodsCategory',
+      })
       this.setData({
         asideData: data.message,
         activeAsideItem: data.message[0]
