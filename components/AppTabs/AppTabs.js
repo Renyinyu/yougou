3cbf57b560e0a6adc4e60a30b2ef74c623555375
renyinyu <471630805@qq.com>
2020-12-17 22:49:40 +0800

    'add'

---
+++ components/AppTabs/AppTabs.js
@@ -0,0 +1,27 @@
+// components/AppTabs/AppTabs.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tabs: [
+      {id: 1, title: 'tab 1'},
+      {id: 2, title: 'tab 2'},
+      {id: 3, title: 'tab 3'},
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
